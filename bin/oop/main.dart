d76453cd0a0b92903afa353ecb657d483f9ca378
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-08-02 18:56:03 +0800

    feat

--- bin/oop/main.dart
+++ bin/oop/main.dart
@@ -8,4 +8,11 @@ void main(){
   animal.name = 'Kucing Sedang' ;
   animal.eat(1.0); // Kucing is eating 1.0 kg
   print(animal.weight); // 22.5
+
+  // cascade operator
+  Animal dog = Animal('Anjing', 5, 30)
+    ..eat(3.0) // Anjing is eating 3.0 kg of food. New weight: 33.0 kg
+    ..name = 'Anjing Besar'
+    ..eat(2.0); // Anjing Besar is eating 2.0
+  print(dog.weight); // 33.0
 }
