4c0eebd506f95c282d8b8ea795accab531195a63
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-08-05 14:16:03 +0800

    higher orser function

---
+++ bin/oop/higher_order_function.dart
@@ -0,0 +1,21 @@
+void main() {
+  // Opsi 1
+  sum(int num1, int num2) => num1 + num2;
+  myHigherOrderFunction('Hello', sum);
+
+  // Opsi 2
+  myHigherOrderFunction('Hello', (num1, num2) => num1 + num2);
+
+  var fibonacci = [0, 1, 1, 2, 3, 5, 8, 13];
+  fibonacci.forEach((element) {
+    print(element);
+  });
+}
+
+void myHigherOrderFunction(
+    String message,
+    int Function(int num1, int num2) myFunction,
+    ) {
+  print(message);
+  print(myFunction(3, 4));
+}
